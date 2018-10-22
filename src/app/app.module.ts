ff264ad7c0523c8d31e4862bd1e43e9200766fdb
Eguerrero86 <eguerrero86@unab.edu.co>
2018-10-21 19:19:02 -0500

    Revert "Revert "Merge branch 'master' into edwin""
    This reverts commit 7bf0b2bd4ad54300735ce73fe8d9c68ee719f772.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule,Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
@@ -11,6 +11,7 @@ import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-p
 import { OfertaComponent } from './components/oferta/oferta.component';
 import { DemandaComponent } from './components/demanda/demanda.component';
 import { FooterComponent } from './components/footer/footer.component';
+<<<<<<< HEAD
 import { PerfilConsumidorComponent } from './components/perfil-consumidor/perfil-consumidor.component';
 //Animations
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -41,6 +42,21 @@ const appRoutes: Routes =[
   {
     path: 'perfil-consumidor', component: PerfilConsumidorComponent
   }
+=======
+import { HeaderComponent } from './components/header/header.component';
+import { ConsumidorService } from './services/consumidor/consumidor.service';
+import { HttpClientModule } from "@angular/common/http";
+import { FormComponent } from './components/registro-consumidor/form.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'registro', component: RegistroConsumidorComponent },
+  { path: 'inicioSesion', component: LoginComponent },
+  { path: 'oferta', component: OfertaComponent },
+  { path: 'demanda', component: DemandaComponent },
+  { path: 'registro/form', component: FormComponent },
+  { path: 'registro/form/:cedula_consumidor', component: FormComponent }
+>>>>>>> master
 ]
 @NgModule({
   declarations: [
@@ -52,11 +68,17 @@ const appRoutes: Routes =[
     OfertaComponent,
     DemandaComponent,
     FooterComponent,
+<<<<<<< HEAD
     PerfilConsumidorComponent,
     BarraNavegacionComponent,
+=======
+    HeaderComponent,
+    FormComponent
+>>>>>>> master
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     MaterialModule,
     BrowserAnimationsModule,
@@ -65,7 +87,7 @@ const appRoutes: Routes =[
       { enableTracing: true } // <-- debugging purposes only
     )
   ],
-  providers: [],
+  providers: [ConsumidorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/demanda/demanda.component.ts
+++ src/app/components/demanda/demanda.component.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Demanda } from '../../models/demanda';
+import { Router, ActivatedRoute } from "@angular/router";
+import { DemandaService } from '../../services/demanda/demanda.service';
+import swal from 'sweetalert2'
 @Component({
   selector: 'app-demanda',
   templateUrl: './demanda.component.html',
   styleUrls: ['./demanda.component.css']
 })
 export class DemandaComponent implements OnInit {
-
-  constructor() { }
+  demandas: Demanda[];
+  private demanda: Demanda = new Demanda();
+  constructor(private demandaService: DemandaService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.demandaService.getDemandas().subscribe((demandas)=>{
+      this.demandas=demandas
+    })
   }
+
+  cargarDemanda(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id_demanda = params['id_demanda'];
+      if (id_demanda) {
+        this.demandaService.getDemanda(id_demanda)
+          .subscribe((demanda) => this.demanda = demanda)
+      }
+    })
+  }
+
+  crearDemanda(): void {
+    this.demandaService.crearDemanda(this.demanda)
+    .subscribe(demanda =>{
+      this.router.navigate(['/demanda'])
+      swal("Demanda publicada", `${demanda.nombre_producto} Publicada con éxito`, "success");
+        this.demanda = new Demanda();
+    })
+  }
+
+  ActualizarDemanda(): void {
+    this.demandaService.updateDemanda(this.demanda)
+    .subscribe( demanda =>{
+      this.router.navigate(['/registro'])
+      swal("Actualización exitosa", `${demanda.nombre_producto} fue actualizado`, "success");
+    })
+  }
+
+
+  eliminarDemanda(demanda: Demanda){
+    swal({
+      title: '¿Está seguro?',
+      text:`Seguro que quiere eliminar a ${demanda.nombre_producto}`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: 'red',
+      cancelButtonColor: 'No, cancelar',
+      confirmButtonText: 'Si,eliminar'
+    }).then((result) => {
+      if (result.value) {
+        this.demandaService.deleteDemanda(demanda.id_demanda)
+        .subscribe( response =>{
+          this.demandas = this.demandas.filter(con => con !== demanda);
+        swal(
+          'Usuario eliminado',
+          `${demanda.nombre_producto} eliminado con éxito`,
+          'success'
+        )
+      })
+      }
+    })
+
+  }
+
+
 
 }
